d6cd983a47dcf9acc8fdf22df9b1825fd7b43f1d
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-30 17:19:09 +0100

    State

--- app/javascript/controllers/country_controller.js
+++ app/javascript/controllers/country_controller.js
@@ -12,8 +12,7 @@ export default class extends Controller {
    static targets = ["stateSelect"]
    change(event) {
       let country = event.target.selectedOptions[0].value;
-      let target = this.stateSelectTarget.id
-      //let target = this.stateSelectTarget.id
+      let target = this.stateSelectTarget.id;
       get(`/addresses/states?target=${target}&country=${country}`, {
          responseKind: "turbo-stream"
       });
